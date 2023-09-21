7b08ea3eec75c745c27e44f3ea2c91580c8fa730
Shen <shenhoei@eikeconsulting.com>
2023-09-21 22:02:22 +0800

    Work 2023-09-21
    Fixing side menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554",'summitblue':'#4C6FFF','offblue':'#DFE6FB','highlightblue':'#EDF1FF'}
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554",'summitblue':'#4C6FFF','summitlight':'#EDF1FF','summitdarkblue':'#234EFF','summit-mainbgblue':'#DFE6FB','highlightblue':'#EDF1FF'}
       }
     },
     fontFamily: {
