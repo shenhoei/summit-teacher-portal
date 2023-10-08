ac557d48d999b847e8a57b49bbf6f691c5e69ae5
Shen <shenhoei@eikeconsulting.com>
2023-10-08 23:39:43 +0800

    End of day commit 2023-10-08
    Fixed a lot of menu and items

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554",'summitblue':'#4C6FFF','summitlight':'#EDF1FF','summitdarkblue':'#234EFF','summit-mainbgblue':'#DFE6FB','summit-highlightblue':'#EDF1FF','summit-maintext':'#858585','summit-bordergray':'#d9d9d9','summit-border-lightgray':'#e4e4e4'}
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554",'summitblue':'#4C6FFF','summitlight':'#EDF1FF','summitdarkblue':'#234EFF','summit-mainbgblue':'#DFE6FB','summit-highlightblue':'#EDF1FF','summit-maintext':'#858585','summit-bordergray':'#d9d9d9','summit-border-lightgray':'#e4e4e4','summit-button-gray':'#7a7a7a'}
       }
     },
     fontFamily: {
