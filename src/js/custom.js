ecb3ed3b750d1ff392d459995be643e923dd1be0
Shen <shenhoei@eikeconsulting.com>
2023-10-24 22:12:39 +0800

    2023-10-24 Video player fixes
    Fix for the MP4 video player

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
-    $("#asideToggleBtn,#asideToggleBtn2").click(function() {
-      $(".asideBarToggle").toggleClass("custom-translate-x-full");
-      $(".bg-primary-summit-mainbgblue").toggleClass("removeMarginFromMain");
-      $("#asideToggleBtn svg").toggleClass("arrowRotate");
-    });
+$(document).ready(function () {
+  $("#asideToggleBtn,#asideToggleBtn2").click(function () {
+    $(".asideBarToggle").toggleClass("custom-translate-x-full");
+    $(".bg-primary-summit-mainbgblue").toggleClass("removeMarginFromMain");
+    $("#asideToggleBtn svg").toggleClass("arrowRotate");
+  });
 
-    $("#modalclose3").click(function () {
-      $("#videoplayer3").attr('src',$('#videoplayer3').attr('src')); 
-      });
+  $("#modalclose3").click(function () {
+    $("#videoplayer3").attr("src", $("#videoplayer3").attr("src"));
+  });
+  $("#modalclose2").click(function () {
+    $('#mp4player')[0].pause(); 
+  });
 });
