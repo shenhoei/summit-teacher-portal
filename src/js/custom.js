1bd7997d9ae35ff249ba5406c44d1e5baadf0c38
Shen <shenhoei@eikeconsulting.com>
2024-06-04 00:25:44 +0800

    Major tweak.
    Fixed colors, changed and added new pages.

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#asideToggleBtn,#asideToggleBtn2").click(function () {
     $(".asideBarToggle").toggleClass("custom-translate-x-full");
-    $(".bg-primary-summit-mainbgblue").toggleClass("removeMarginFromMain");
+    $(".bg-summit-blue-light").toggleClass("removeMarginFromMain");
     $("#asideToggleBtn svg").toggleClass("arrowRotate");
   });
 
