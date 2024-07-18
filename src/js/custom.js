13716bcbdf7a47608022893594263595da623562
Shen <shenhoei@eikeconsulting.com>
2024-07-18 14:51:45 +0800

    Noice

--- src/js/custom.js
+++ src/js/custom.js
@@ -16,4 +16,22 @@ $(document).ready(function () {
     $(this).toggleClass("asideRotate")
   });
 
+  $(".half-circle-progress").each(function(){
+  
+    var $bar = $(this).find(".half-circle-bar");
+    var $val = $(this).find("span.half-circle-percent");
+    var perc = parseInt( $val.text(), 10);
+  
+    $({p:0}).animate({p:perc}, {
+      duration: 1000,
+      easing: "swing",
+      step: function(p) {
+        $bar.css({
+          transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
+          // 45 is to add the needed rotation to have the green borders at the bottom
+        });
+        $val.text(p|0);
+      }
+    });
+  });
 });
