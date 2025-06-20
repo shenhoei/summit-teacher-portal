77ddc38dc57bd440fdd29abcd287291537b4e726
Shen <shenhoei@eikeconsulting.com>
2025-06-20 10:02:01 +0800

    What the heck

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
     $("#asideToggleBtn img").toggleClass("arrowRotate");
     $("#teacher-reports-content").toggleClass("teacher-report-main-margin");
     var asideVal = $("aside").width();
-    if($(".teacher-report-main-margin")[0]){
-    $("#summit-table-holder").width($("main").width() - 42);
-    } else{
-      $("#summit-table-holder").width($("main").width() - asideVal - 42); 
+    if ($(".teacher-report-main-margin")[0]) {
+      $("#summit-table-holder").width($("main").width() - 42);
+    } else {
+      $("#summit-table-holder").width($("main").width() - asideVal - 42);
     }
   });
 
@@ -20,8 +20,8 @@ $(document).ready(function () {
     $("#student-plp-content").toggleClass("student-plp-main-margin");
   });
 
-  
-  $(function(){
+
+  $(function () {
     $("#summit-table-holder").width($("main").width() - $("aside").width() - 42);
   });
 
@@ -29,30 +29,109 @@ $(document).ready(function () {
     $("#videoplayer3").attr("src", $("#videoplayer3").attr("src"));
   });
   $("#modalclose2").click(function () {
-    $('#mp4player')[0].pause(); 
+    $('#mp4player')[0].pause();
   });
-  $(".aside-button").click(function(){
+  $(".aside-button").click(function () {
     $(this).toggleClass("asideRotate")
   });
 
-  $(".half-circle-progress").each(function(){
-  
+  $(".half-circle-progress").each(function () {
+
     var $bar = $(this).find(".half-circle-bar");
     var $val = $(this).find("span.half-circle-percent");
-    var perc = parseInt( $val.text(), 10);
-  
-    $({p:0}).animate({p:perc}, {
+    var perc = parseInt($val.text(), 10);
+
+    $({ p: 0 }).animate({ p: perc }, {
       duration: 1000,
       easing: "swing",
-      step: function(p) {
+      step: function (p) {
         $bar.css({
-          transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
+          transform: "rotate(" + (45 + (p * 1.8)) + "deg)", // 100%=180° so: ° = % * 1.8
           // 45 is to add the needed rotation to have the green borders at the bottom
         });
-        $val.text(p|0);
+        $val.text(p | 0);
       }
     });
   });
 
   $("#defaultModal").modal.isVisible();
+
+  $('.parent-accordian-container').each(function () {
+    const $container = $(this);
+    const key = $container.data('key'); // e.g. 'accordion_boy'
+    const $table = $container.closest('table');
+    const fullCookieKey = `accordion_${key}_activeIndexes`;
+
+    const headers = $(`.parent-accordian-container[data-key="${key}"]`)
+      .closest(`table`)
+      .find('.accordion-header');
+
+    // Restore state from cookie
+    const savedIndexesStr = getCookie(fullCookieKey);
+    let openIndexes = savedIndexesStr ? savedIndexesStr.split(',').map(Number) : [];
+
+    // If no state saved, open index 0
+    if (openIndexes.length === 0 && headers.length > 0) {
+      openIndexes = [0];
+    }
+
+    // Open saved (or default) accordions
+    headers.each(function (i) {
+      if (openIndexes.includes(i)) {
+        const $icon = $(this).find('.accordion-icon');
+        $container.addClass('accordion-open');
+        let $next = $(this).next();
+        while ($next.length && $next.hasClass('accordion-content')) {
+          $next.show();
+          $next = $next.next();
+        }
+      } else {
+        const $icon = $(this).find('.accordion-icon');
+        $container.removeClass('accordion-open');
+        $icon.addClass('rotate-180');
+        $container.addClass('accordion-close');
+        let $next = $(this).next();
+        while ($next.length && $next.hasClass('accordion-content')) {
+          $next.hide();
+          $next = $next.next();
+        }
+
+      }
+    });
+
+    // Click handler for toggling
+    headers.each(function (i) {
+      $(this).click(function () {
+        const isOpen = openIndexes.includes(i);
+
+        // Toggle current header
+        const $icon = $(this).find('.accordion-icon');
+        let $next = $(this).next();
+        while ($next.length && $next.hasClass('accordion-content')) {
+          if (isOpen) {
+            $next.hide();
+          } else {
+            $next.show();
+          }
+          $next = $next.next();
+        }
+
+        // Update icon
+        if (isOpen) {
+          $container.removeClass('accordion-open');
+          $icon.addClass('rotate-180');
+          $container.addClass('accordion-close');
+          openIndexes = openIndexes.filter(index => index !== i);
+        } else {
+          $container.addClass('accordion-open');
+          $icon.removeClass('rotate-180');
+          $container.removeClass('accordion-close');
+          openIndexes.push(i);
+        }
+
+        // Save updated state
+        setCookie(fullCookieKey, openIndexes.join(','));
+      });
+    });
+  });
 });
